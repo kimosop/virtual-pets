af03fb3ab0d28b45e32e287c5f31e2cf94716590
VCHEPKOIT <vchepkoit@safaricom.co.ke>
2019-05-27 17:23:54 +0300

    person.java constructors

---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,39 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.sql2o.*;
+
+public class PersonTest {
+
+    @Test
+    public void person_instantiatesCorrectly_true() {
+        Person testPerson = new Person("Henry", "[email protected]");
+        assertEquals(true, testPerson instanceof Person);
+    }
+
+    @Test
+    public void getName_personInstantiatesWithName_Henry() {
+        Person testPerson = new Person("Henry", "[email protected]");
+        assertEquals("Henry", testPerson.getName());
+    }
+
+    @Test
+    public void getEmail_personInstantiatesWithEmail_String() {
+        Person testPerson = new Person("Henry", "[email protected]");
+        assertEquals("[email protected]", testPerson.getEmail());
+    }
+
+    @Test
+    public void equals_returnsTrueIfNameAndEmailAreSame_true() {
+        Person firstPerson = new Person("Henry", "[email protected]");
+        Person anotherPerson = new Person("Henry", "[email protected]");
+        assertTrue(firstPerson.equals(anotherPerson));
+    }
+
+    @Test
+    public void save_insertsObjectIntoDatabase_Person() {
+        Person testPerson = new Person("Henry", "[email protected]");
+        testPerson.save();
+        assertTrue(Person.all().get(0).equals(testPerson));
+    }
+
+}
