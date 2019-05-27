c1eab571bf87e2645755ddd84072ac096858d7e2
VCHEPKOIT <vchepkoit@safaricom.co.ke>
2019-05-27 16:55:33 +0300

    person.java constructors

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,2 +1,19 @@
 public class Person {
+    private String name;
+    private String email;
+
+    public Person(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
 }
